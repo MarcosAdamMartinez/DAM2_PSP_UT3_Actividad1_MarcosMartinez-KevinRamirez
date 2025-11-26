a1d3f5e2f0499e2d7ab538e0de978e3226223daa
bananaboy448 <zerobananaboy@gmail.com>
2025-11-26 21:02:39 +0100

    Cambios en la clase HilosIP

--- src/main/java/HilosIP.java
+++ src/main/java/HilosIP.java
@@ -1,9 +1,17 @@
+import java.net.Socket;
+
 public class HilosIP {
 
     String ip;
 
     public HilosIP(String ip) {
         this.ip = ip;
+    }
+
+    public static void mostrarPuertosArriba(String ip) {
+
+        Socket socket = new Socket();
+
     }
 
 }
